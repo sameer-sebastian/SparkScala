f031e5230e57931e0193450da3f540c3af6f3c62
sameer-sebastian <sameer.sebastian.git@gmail.com>
2020-01-28 23:46:21 +0530

    Initial commit

---
+++ src/main/scala/com/basics/HelloSparkScala.scala
@@ -0,0 +1,11 @@
+package com.basics
+
+object HelloSparkScala {
+  def main(a: Array[String]): Unit ={
+    print("\n\n>>>>> START OF PROGRAM <<<<<\n\n")
+
+    println("Hello World.")
+
+    print("\n\n>>>>> END OF PROGRAM <<<<<\n\n")
+  }
+}
